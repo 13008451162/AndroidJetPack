0b502ebadbd4a471ce26eafd765627721621dfb9
lukecc0 <s1793026645@163.com>
2024-01-13 22:39:28 +0800

    初始化仓库
    第一次测试+4

---
+++ app/src/main/java/com/xupt3g/androidjetpack/MainActivity.java
@@ -0,0 +1,21 @@
+package com.xupt3g.androidjetpack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+<<<<<<< HEAD
+=======
+        Log.e("TAG", "onCreate: "+1);
+        Log.e("TAG", "onCreate: "+2);
+        Log.e("TAG", "onCreate: "+3);
+        Log.e("TAG", "onCreate: "+4);
+>>>>>>> da4cb29 (第一次测试+4)
+    }
+}
