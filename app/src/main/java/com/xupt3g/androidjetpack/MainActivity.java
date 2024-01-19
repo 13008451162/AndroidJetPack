76e26414815648550dd3f82c6493cb45ee74ff51
lukecc0 <s1793026645@163.com>
2024-01-20 00:21:37 +0800

    测试diff

--- app/src/main/java/com/xupt3g/androidjetpack/MainActivity.java
+++ app/src/main/java/com/xupt3g/androidjetpack/MainActivity.java
@@ -10,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
-        Log.e("TAG", "onCreate: "+1);
-        Log.e("TAG", "onCreate: "+2);
-        Log.e("TAG", "onCreate: "+3);
-        Log.e("TAG", "onCreate: "+4);
->>>>>>> da4cb29 (第一次测试+4)
     }
 }
