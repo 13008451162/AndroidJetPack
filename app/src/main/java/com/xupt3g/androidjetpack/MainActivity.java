718cb8a4a919e512519c8caee9db1a20a208b8a1
lukecc0 <s1793026645@163.com>
2024-01-20 17:05:21 +0800

    111

--- app/src/main/java/com/xupt3g/androidjetpack/MainActivity.java
+++ app/src/main/java/com/xupt3g/androidjetpack/MainActivity.java
@@ -3,6 +3,7 @@ package com.xupt3g.androidjetpack;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.LocaleList;
 
 public class MainActivity extends AppCompatActivity {
 
